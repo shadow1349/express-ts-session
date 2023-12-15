a73f263eac89afb487cab4fc8d4a3808cee3d2ca
Sam Redmond <sredmond@techusolutions.com>
2023-12-15 11:35:17 -0600

    Updating the store model

--- src/models/store.model.ts
+++ src/models/store.model.ts
@@ -1,5 +1,6 @@
 import { Request } from "express";
 import { SessionDataModel, SessionModel } from "./sesson.model";
+import { Session } from "../classes";
 
 export interface StoreModel {
   generate?: (req: Request) => void | Promise<void>;
@@ -8,12 +9,12 @@ export interface StoreModel {
   createSession?: (
     req: Request,
     session: SessionModel
-  ) => SessionModel | Promise<SessionModel>;
-  get: (sid: string) => SessionModel | Promise<SessionModel>;
+  ) => Session | Promise<Session>;
+  get: (sid: string) => SessionDataModel | Promise<SessionDataModel>;
   set: (sid: string, session: SessionDataModel) => void;
   destroy: (sid: string) => void | Promise<void>;
   all?: () => void | Promise<void>;
-  length?: () => void | Promise<void>;
+  length?: () => number | Promise<number>;
   clear?: () => void | Promise<void>;
   touch?: () => void | Promise<void>;
 }
