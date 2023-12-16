0fab0cc8b004fd7ecdb981afeb0db823c25c2228
Sam Redmond <sredmond@techusolutions.com>
2023-12-16 10:36:35 -0600

    removing test code

--- src/index.ts
+++ src/index.ts
@@ -2,33 +2,3 @@ export * from "./classes";
 export * from "./middleware";
 export * from "./models";
 export * from "./util";
-
-import express from "express";
-import { Cookie } from "./classes";
-import { ExpressTSSession } from "./middleware";
-
-const app = express();
-
-const session = new ExpressTSSession({
-  secret: "test",
-  cookie: new Cookie({
-    maxAge: 1000 * 60 * 60 * 24 * 7,
-    secure: false,
-    httpOnly: true,
-    path: "/",
-    sameSite: false,
-    signed: true,
-  }),
-  name: "test",
-});
-
-app.use(session.init);
-
-app.get("/", (req, res) => {
-  req.session["test"] = "123";
-  res.json({ response: "Hello World" });
-});
-
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
